6e8f9dbd77b2a44b5ba904c8645dc3c8daf844ee
kimseoyoungk <99073581+kimseoyoungk@users.noreply.github.com>
2022-02-24 20:57:27 +0900

    Update story05.lua

--- story05.lua
+++ story05.lua
@@ -31,7 +31,7 @@ audio.play( explosionSound )
 function scene:create( event )
 	local sceneGroup = self.view
 	
-	local background = display.newImage( "Content/PNG/script/배경/산속.png")
+	local background = display.newImage( "Content/PNG/script/background/산속.png")
 	background.x, background.y = display.contentWidth/2, display.contentHeight/2
 
 	local section = display.newRect(display.contentWidth/2, display.contentHeight*0.8, display.contentWidth, display.contentHeight*0.3)
